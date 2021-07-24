436a9bb59eab1b4b5a58d15386c972da7fd01919
Deta Open Source <2080315360@qq.com>
2021-07-24 18:13:41 +0800

    排序的字符串部分用map 替换了
    稍后进行 表格搜索的完整句型权重*2，文字的游标搜索更准确
    
    罗瑶光

---
+++ 2021/07/24/Double_ESU.java
@@ -0,0 +1,36 @@
+package ESU.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Double_ESU{
+	public static Map<Double, Map<String, Boolean>> getMapFromDoubleStringArray(Double[] score, String[] nameScore){
+		Map<Double, Map<String, Boolean>> map= new HashMap<>();
+		for(int i= 0; i< score.length; i++) {
+			Map<String, Boolean> maps;
+			if(map.containsKey(score[i])) {
+				maps= map.get(score[i]);
+			}else {
+				maps= new HashMap<>();
+			}
+			maps.put(nameScore[i].toString(), true);
+			map.put(score[i], maps);
+		}
+		return map;
+	}
+	
+	public static Map<Integer, Map<String, Boolean>> getMapFromIntStringArray(int[] score, String[] nameScore){
+		Map<Integer, Map<String, Boolean>> map= new HashMap<>();
+		for(int i= 0; i< score.length; i++) {
+			Map<String, Boolean> maps;
+			if(map.containsKey(score[i])) {
+				maps= map.get(score[i]);
+			}else {
+				maps= new HashMap<>();
+			}
+			maps.put(nameScore[i].toString(), true);
+			map.put(score[i], maps);
+		}
+		return map;
+	}
+}
