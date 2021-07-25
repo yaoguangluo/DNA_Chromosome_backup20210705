ef86ef87883d610ca94ddcfa194a8a6c92ef3df0
Deta Open Source <2080315360@qq.com>
2021-07-25 13:03:25 +0800

    养疗经图片读脏综合组件设计 今日备份
    修改了下继承函数的root 增加新 overwriter overloader接口。
    
    罗瑶光

---
+++ 2021/07/25/AddPGSearchPageNodeASQ_OCQ_OSI_PCI_PCU_MCI_MCU_MSI.java
@@ -0,0 +1,92 @@
+package OSI.OSU.SI.OVI.OSI.AVI.AEI.ACI.ASI.OVI.OEI.OCI.OSI.PVI.PEI.PCI.PSI.addPGSearchPage;
+import java.awt.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import javax.swing.*;
+
+import ME.APM.VSQ.App;
+//import OCI.ME.analysis.C.A;
+import OEI.ME.analysis.E.CogsBinaryForest_AE;
+import OSI.OPE.AOPM.VECS.IDUQ.OVU.PQE.flash.ThisCanvas;
+import OSI.OPE.OEQ.MCQ.OVU.PQE.osgi.*;
+
+public class AddPGSearchPageNodeASQ_OCQ_OSI_PCI_PCU_MCI_MCU_MSI extends OSU_AVQ_ASQ_ASQ_OCQ_OSI_PCI_PCU_MCI_MCU_MSI{	
+	MakeContainerYXTP c4;
+	public AddPGSearchPageNodeASQ_OCQ_OSI_PCI_PCU_MCI_MCU_MSI() throws IOException{
+		thisIcon= new ImageIcon(this.getClass().getResource("addPGSearchPage.jpg"));
+		SQ_OSU_MSQ_OSU_AVQ_ASQ_SQ_VPC_PCS= new String("Í¼Æ¬ËÑË÷½Úµã");
+		AMV_MVS_VSQ= new String("MEDCINE");
+		Image img= ((ImageIcon) thisIcon).getImage();
+		Image newimg= img.getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH);
+		thisImage= img.getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH );
+		thisIcon= new ImageIcon(newimg);
+		//
+	}
+	
+	public AddPGSearchPageNodeASQ_OCQ_OSI_PCI_PCU_MCI_MCU_MSI(Object[][] tableData_old, JTextPane text, App u
+			, CogsBinaryForest_AE _A, Map<String, String> pos) throws IOException{
+		this.text= text;
+		this.tableData_old= tableData_old;
+		thisIcon= new ImageIcon(this.getClass().getResource("addPGSearchPage.jpg"));
+		SQ_OSU_MSQ_OSU_AVQ_ASQ_SQ_VPC_PCS= new String("Í¼Æ¬ËÑË÷½Úµã");
+		AMV_MVS_VSQ= new String("MEDCINE");
+		Image img= ((ImageIcon) thisIcon).getImage();
+		Image newimg= img.getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH);
+		thisImage= img.getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH );
+		thisIcon= new ImageIcon(newimg);
+		//
+		this.u= u;
+		this._A= _A;
+		this.pos= pos;
+	}
+	public void MEI_MSU(JTextPane jTextPane, ThisCanvas canvas) throws IOException{
+		SQ_OSU_MSQ_OSU_AVQ_ASQ_AVQ_ASQ_OVQ_OSQ_VSQ= new AddPGSearchPageOSU_MSQ_AVQ_ASQ_OVQ_OSQ_VSQ();
+		SQ_OSU_MSQ_OSU_AVQ_ASQ_OPE_OPC_ECI= new AddPGSearchPageOSU_MSQ_OPE_OPC_ECI();
+		SQ_OSU_MSQ_OSU_AVQ_ASQ_AOI_AOD_AOU_AOQ_VES= new AddPGSearchPageOSU_MSQ_AOI_AOD_AOU_AOQ_VES(
+				(AddPGSearchPageOSU_MSQ_OPE_OPC_ECI) SQ_OSU_MSQ_OSU_AVQ_ASQ_OPE_OPC_ECI, this.text, this.tableData_old);
+		SQ_OSU_MSQ_OSU_AVQ_ASQ_AOI_AOD_AOU_AOQ_VES.config();
+		showed= false;
+	}
+	public void IMP_PSU() throws IOException {
+		Container jpanelFourth= new Container();
+		c4= new MakeContainerYXTP(_A, jpanelFourth, u, u.jTabbedpane, u.tabNames
+				, pos, _A.getPosEnToEn(), _A.getPosEnToCn(), _A.getCnToEn());
+		c4.start();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		u.jTabbedpane.validate();
+	}
+	
+	public void AOP_VEC_SQ(JTextPane rightBotJTextPane, String input) throws IOException{
+		c4.AOP_VEC_SQ(input);
+	}
+	
+	public void AOP_VEC_SQ_PDE(JTextPane rightBotJTextPane, List<String> pathRankList) throws IOException{
+		c4.AOP_VEC_SQ_PDE(pathRankList);
+	}
+	
+	public void OPE_E(JTextPane jTextPane) throws FileNotFoundException, IOException{	
+		((AddPGSearchPageOSU_MSQ_OPE_OPC_ECI) SQ_OSU_MSQ_OSU_AVQ_ASQ_OPE_OPC_ECI)
+		.run((AddPGSearchPageOSU_MSQ_AVQ_ASQ_OVQ_OSQ_VSQ) SQ_OSU_MSQ_OSU_AVQ_ASQ_AVQ_ASQ_OVQ_OSQ_VSQ);
+	}
+	public void AVQ_ASQ_OVQ_OSQ_VSQ(JTextPane jTextPane) throws Exception{
+		((AddPGSearchPageOSU_MSQ_AVQ_ASQ_OVQ_OSQ_VSQ) SQ_OSU_MSQ_OSU_AVQ_ASQ_AVQ_ASQ_OVQ_OSQ_VSQ)._A= _A;
+		((AddPGSearchPageOSU_MSQ_AVQ_ASQ_OVQ_OSQ_VSQ) SQ_OSU_MSQ_OSU_AVQ_ASQ_AVQ_ASQ_OVQ_OSQ_VSQ).u= u;
+		((AddPGSearchPageOSU_MSQ_AVQ_ASQ_OVQ_OSQ_VSQ) SQ_OSU_MSQ_OSU_AVQ_ASQ_AVQ_ASQ_OVQ_OSQ_VSQ).pos= pos;
+		((AddPGSearchPageOSU_MSQ_AVQ_ASQ_OVQ_OSQ_VSQ) SQ_OSU_MSQ_OSU_AVQ_ASQ_AVQ_ASQ_OVQ_OSQ_VSQ).DNNtext
+		= ((AddPGSearchPageOSU_MSQ_AOI_AOD_AOU_AOQ_VES) SQ_OSU_MSQ_OSU_AVQ_ASQ_AOI_AOD_AOU_AOQ_VES).DNNtext;
+		SQ_OSU_MSQ_OSU_AVQ_ASQ_AVQ_ASQ_OVQ_OSQ_VSQ.view();
+		showed = true;
+	}
+	public OSU_AVQ_ASQ_ASQ_OCQ_OSI_PCI_PCU_MCI_MCU_MSI AOP_MVE_CSI_DUQ() throws CloneNotSupportedException, IOException{
+		SQ_OSU_MSQ_OSU_AVQ_ASQ_ASQ_OCQ_OSI_PCI_PCU_MCI_MCU_MSI 
+		= new AddPGSearchPageNodeASQ_OCQ_OSI_PCI_PCU_MCI_MCU_MSI(this.tableData_old, this.text, this.u, this._A, this.pos);   
+		return SQ_OSU_MSQ_OSU_AVQ_ASQ_ASQ_OCQ_OSI_PCI_PCU_MCI_MCU_MSI;  
+	}
+}
