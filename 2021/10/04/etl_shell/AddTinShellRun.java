56a3e7036aac50215ddce0650615a0b7429d5ad0
Deta Open Source <2080315360@qq.com>
2021-10-07 01:18:50 +0800

    稍后我会在父类 添加一个map 变量，这样就支持 文章流计算了
    罗瑶光

---
+++ 2021/10/04/etl_shell/AddTinShellRun.java
@@ -0,0 +1,17 @@
+package OSI.OSU.SI.ASQ.OSD.AVI.AEI.ACI.ASI.OVI.OEI.OCI.OSI.PVI.PEI.PCI.PSI.tinShell;
+import java.io.IOException;
+
+import OSI.OPE.OEQ.MCQ.OVU.PQE.osgi.*;
+
+public class AddTinShellRun extends OSU_AVQ_ASQ_OPE_OPC_ECI{
+	private static final long serialVersionUID = 1L;
+	public int value = 0;
+	public String filepath;
+	public AddTinShellRun( ) throws IOException{	
+		super();
+	}
+	
+	public void run(final AddTinShellView SQ_OSU_MSQ_OSU_AVQ_ASQ_AVQ_ASQ_OVQ_OSQ_VSQ) throws IOException{
+		 SQ_OSU_MSQ_OSU_AVQ_ASQ_AVQ_ASQ_OVQ_OSQ_VSQ.tableout = this.toptablein; 	
+	}
+}
