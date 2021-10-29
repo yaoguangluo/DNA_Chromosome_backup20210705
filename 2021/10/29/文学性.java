6c0ad811d1d4fccbc9279e564f472c825ad26620
Deta Open Source <2080315360@qq.com>
2021-10-29 12:33:06 +0800

    文言文搜索设计中。
    1 以后这里词语多了，就设计map索引来替换。
    2 我在思考，搜索替换模式，不如替换原文解释，
    替换原文为白话文，文字处理长度虽然翻倍。但是更准确。降低时间换准确性。
    3 觉得有必要， 那词库就设计在智慧阅读里面。稍后开始设计。
    
    4 稍后这个词库用于文言文搜索选项。
    5 完全有必要性。距离 发物/过敏源发物 ，如果搜索过敏，那么很好 分词的时候连带出发字
    6  如果搜 发字 有可能是头发， 只有 发物 词汇的 匹配才出过敏源。 类似一种充要条件。
    稍后这个map 词库用在文言文的搜索打分时 词汇替换。
    7 上个星期 搜索优化 速度提升了2倍，刚好这个性能提升利益用来让给这些精度搜索组件。
    
    罗瑶光

---
+++ 2021/10/29/文学性.java
@@ -0,0 +1,50 @@
+package ME.APM.VSQ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+//准备导入数据
+//罗瑶光
+//20211028
+public class 文学性{
+	public static List<String> 文学词库;
+	public static void 初始词库() throws IOException {
+		if(null== 文学词库) {
+			文学词库= new ArrayList<>();
+			InputStream in= new 文学性().getClass().getResourceAsStream("BToW.lyg");
+			BufferedReader cReader= new BufferedReader(new InputStreamReader(in));  
+			String ctempString= null; 
+			while ((ctempString= cReader.readLine()) != null) {  
+				if(!ctempString.replace(" ","").equals(""))
+					文学词库.add(ctempString);
+			}
+			cReader.close();
+		}
+	}
+
+	public static String 文字处理(App app, String input) {
+		List<String> list= app._A.parserMixedString(input);
+		Iterator<String> iteratorList= list.iterator();
+		StringBuilder stringBuilder= new StringBuilder();
+		Here:
+		while(iteratorList.hasNext()) {
+			String stringIteratorList= iteratorList.next();
+			if(1< stringIteratorList.length()) {
+				Iterator<String> iterator= 文学词库.iterator();
+				while(iterator.hasNext()) {
+					String string= iterator.next();
+					if(string.contains(stringIteratorList)) {
+						stringBuilder.append(string);
+						continue Here;
+					}
+				}	
+			}
+			stringBuilder.append(stringIteratorList);
+		}
+		return stringBuilder.toString();
+	}
+}
