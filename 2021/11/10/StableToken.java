965edad1831910d623f273ed2b3ba517e15bbc17
Deta Open Source <2080315360@qq.com>
2021-11-10 18:23:43 +0800

    设计 Beconnet 改 Beconnect
    涉及Beconnet 改 Beconnect 文件过多，这里不全部加载。以后会完整项目整体 push一次。
    
    罗瑶光

---
+++ 2021/11/10/StableToken.java
@@ -0,0 +1,49 @@
+package SVQ.stable;
+
+public class StableToken{
+	//…‘∫Û∫œ≤¢
+	//session token
+	public static final String EMPTY_STRING= "";
+	public static final String LINE_ENTER= "\r\n";
+	public static final String DNA_TOKEN_LOCK= "LOCK";
+	public static final String DNA_TOKEN_KEY= "KEY";
+	public static final String DNA_TOKEN_PDE= "PDE";
+	public static final String DNA_TOKEN_PDS= "PDS";
+	
+	//etl node token
+	public static final String ETL_NODE_COORDINATION_X= "NodeCoordinationX:";
+	public static final String ETL_NODE_NAME= "NodeName:";
+	public static final String ETL_NODE_COORDINATION_Y= "NodeCoordinationY:";
+	public static final String ETL_NODE_ID= "NodeID:";
+	public static final String ETL_FLASH= "flash:";
+	public static final String ETL_BECONNECT= "beconnect:";
+	public static final String ETL_LEFT_CHOOSE= "leftChoose:";
+	public static final String ETL_RIGHT_CHOOSE= "rightChoose:";
+	public static final String ETL_TBECONNECT= "tBeconnect:";
+	public static final String ETL_TBECONNECT_X= "tBeconnectX:";
+	public static final String ETL_TBECONNECT_Y= "tBeconnectY:";
+	public static final String ETL_TBECONNECT_NAME= "tBeconnectName:";
+	public static final String ETL_TBECONNECT_ID= "tBeconnectID:";
+	public static final String ETL_TBECONNECT_PRIMARY_KEY= "tBeconnectPrimaryKey:";
+	public static final String ETL_MBECONNECT= "mBeconnect:";
+	public static final String ETL_MBECONNECT_X=  "mBeconnectX:";
+	public static final String ETL_MBECONNECT_Y= "mBeconnectY:";
+	public static final String ETL_MBECONNECT_NAME= "mBeconnectName:";
+	public static final String ETL_MBECONNECT_ID= "mBeconnectID:";
+	public static final String ETL_MBECONNECT_PRIMARY_KEY= "mBeconnectPrimaryKey:";
+	public static final String ETL_DBECONNECT= "dBeconnect:";
+	public static final String ETL_DBECONNECT_X= "dBeconnectX:";
+	public static final String ETL_DBECONNECT_Y= "dBeconnectY:";
+	public static final String ETL_DBECONNECT_NAME= "dBeconnectName:";
+	public static final String ETL_DBECONNECT_ID= "dBeconnectID:";
+	public static final String ETL_DBECONNECT_PRIMARY_KEY= "dBeconnectPrimaryKey:";
+	public static final String ETL_PRIMARY_KEY= "primaryKey:";
+	public static final String ETL_NODE_CONFIGURATION= "nodeConfiguration:";
+	public static final String ETL_ISCONFIGED= "isConfiged:";
+	public static final String ETL_ISEXECUTED= "isExecuted:"; 
+	 
+	 
+	 
+	 
+	 
+}
