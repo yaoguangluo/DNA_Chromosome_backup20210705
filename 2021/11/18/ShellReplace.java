6b030cb414eeec43fc570ab87057531e96569b93
Deta Open Source <2080315360@qq.com>
2021-11-18 16:32:44 +0800

    一步一步来
    罗瑶光

---
+++ 2021/11/18/ShellReplace.java
@@ -0,0 +1,44 @@
+package OSM.shell;
+
+public class ShellReplace {
+	//稍后开始
+	//文件替换
+	//罗瑶光
+	public void replaceString(String fileDirectroyPah, String searchString, String needReplaceString) {
+//		1 确定文件夹
+//		2 文件夹文件遍历，
+//		3 全文搜索替换
+	}
+	
+	//文件名对应筛选
+	public void replaceStringWithFileName(String fileDirectroyPah, String searchString, String needReplaceString, String fileName) {
+//		1 确定文件夹
+//		2 文件夹文件遍历，相关文件名
+//		3 全文搜索替换
+	}
+	
+	//文件后缀筛选
+	public void replaceStringWithFileType(String fileDirectroyPah, String searchString, String needReplaceString, String fileType) {
+//		1 确定文件夹
+//		2 文件夹文件遍历，相关文件类型后缀对应
+//		3 全文搜索替换
+	}
+	
+	//文件大小筛选
+	public void replaceStringWithFileSize(String fileDirectroyPah, String searchString, String needReplaceString, long filesize_KB) {
+//		1 确定文件夹
+//		2 文件夹文件遍历，相关文件大小
+//		3 全文搜索替换
+	}
+
+	//文件大小筛选
+	public void replaceStringWithFileLines(String fileDirectroyPah, String searchString, String needReplaceString, long beginLine, long endLine) {
+//		1 确定文件夹
+//		2 文件夹文件遍历，搜索对应的行间
+//		3 全文搜索替换 
+	}
+	
+	public static void main(String[] argv) {
+		
+	}
+}
