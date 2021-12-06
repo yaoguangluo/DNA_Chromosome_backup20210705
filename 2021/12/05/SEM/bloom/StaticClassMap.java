d8792119a4dc134d25843f2bff2e8940e2a1cfc1
Deta Open Source <2080315360@qq.com>
2021-12-06 19:34:03 +0800

    遗传元基索引 开始了。
    罗瑶光

---
+++ 2021/12/05/SEM/bloom/StaticClassMap.java
@@ -0,0 +1,22 @@
+package SEM.bloom;
+import java.util.Map;
+//用来索引文件。
+//罗瑶光
+public class StaticClassMap{
+	public static Map<String, Object>  staticClassMap;
+	public StaticClassMap(String string) {
+		if("A_VECS".equals(string)) {
+			//..遍历工程文件。。。导入注册函数。
+		}
+		
+		if("A_IDUQ".equals(string)) {
+			//..
+		}
+		
+		if("O_VECS".equals(string)) {
+			//..
+		}
+		// TODO Auto-generated constructor stub
+	}
+	
+}
