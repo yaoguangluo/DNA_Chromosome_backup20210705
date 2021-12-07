48250cc48ca59486b2c8eb62317986e943aec9e8
Deta Open Source <2080315360@qq.com>
2021-12-07 09:27:54 +0800

    简单归纳了下。
    1先工程归纳，然后
    2分类，最后
    3统计执行接口的相关java文件，
    4进行map索引。
    
    
    简单归纳了下，准备调整下。就开始分类了。
    
    罗瑶光

---
+++ 2021/12/07/StaticClassMap.java
@@ -0,0 +1,104 @@
+package SEM.bloom;
+//import java.util.ArrayList;
+//import java.util.List;
+import java.util.Map;
+//import java.util.concurrent.ConcurrentHashMap;
+
+//import ASQ.PSU.test.TimeCheck;
+//import AVQ.ASQ.OVQ.OSQ.VSQ.obj.WordFrequency;
+//import OEI.ME.analysis.E.CogsBinaryForest_AE;
+//用来索引文件。
+//罗瑶光
+//流程，1先工程归纳，然后2分类，最后3统计执行接口的相关java文件，4进行map索引。
+public class StaticClassMap{
+	public static Map<String, Object>  staticClassMap;
+	public StaticClassMap(String string) {
+		if("A_VECS".equals(string)) {
+			//..遍历工程文件。。。导入注册函数。
+			//分词 读心术 情感分析，教育分析，文本分析，视觉动画
+			
+		}
+		if("A_IDUQ".equals(string)) {
+			//..肽展公式， dna加密，vpcs服务器， 
+		}
+		
+		if("O_VECS".equals(string)) {
+			//..tinshell， 中文发音，数据库语言，离散余弦变换，
+		}
+		if("O_IDUQ".equals(string)) {
+			//..etl 文档流执行， 保存。
+		}
+		
+		if("P_VECS".equals(string)) {
+			//..数据预测完整包。
+		}
+		if("P_IDUQ".equals(string)) {
+			//..dna遗传杂交组件
+		}
+		
+		if("M_VECS".equals(string)) {
+			//..数据库 增删改查。界面控件，打印插件，三维词花组件
+		}
+		if("M_IDUQ".equals(string)) {
+			//..欧拉 元基进制环路， 元基进制变换。
+		}
+		
+		if("V_AOPM".equals(string)) {
+			//..dna 概率钥匙非对称变换加密， 缓存，三维视觉，
+		}
+		if("V_IDUQ".equals(string)) {
+			//..etl核心组件，界面，etl读取文档，
+		}
+		
+		if("E_AOPM".equals(string)) {
+			//..德塔分词核心组件。
+		}
+		if("E_IDUQ".equals(string)) {
+			//..etl 界面操作组件，类 osgi 插件注册组件
+		}
+		
+		if("C_AOPM".equals(string)) {
+			//..vpcs服务器中心， 自然语言处理组件
+		}
+		if("C_IDUQ".equals(string)) {
+			//..自然语言 控制中心
+		}
+		
+		if("S_AOPM".equals(string)) {
+			//..数据记录中心
+		}
+		if("S_IDUQ".equals(string)) {
+			//..线性，非线性数据操作中心
+		}
+		
+		if("I_AOPM".equals(string)) {
+			//..语音记录，三维数据分析登记
+		}
+		if("I_VECS".equals(string)) {
+			//..肽腐蚀， 非卷积视觉，图片读脏
+		}
+		
+		if("D_AOPM".equals(string)) {
+			//..数据异常处理，
+		}
+		if("D_VECS".equals(string)) {
+			//..数据异常处理，，稍后把功能是 删除的 移到这里来。
+		}
+		
+		if("U_AOPM".equals(string)) {
+			//..卷积变换处理
+		}
+		if("U_VECS".equals(string)) {
+			//..数据变换处理
+		}
+		
+		if("Q_AOPM".equals(string)) {
+			//..六元dna杂交计算框架
+		}
+		if("Q_VECS".equals(string)) {
+			//..dna搜索， 数据库orm， 函数语言
+		}
+		// TODO Auto-generated constructor stub
+	}
+	
+}
