0695b2f9c830b8444ef6741487d5aa45b621eebc
Deta Open Source <2080315360@qq.com>
2021-12-09 15:11:45 +0800

    1 将dna加密的 main test 进行封装成函数A IDUQ组，准备优化下。
    1 简单测试了下， 稍后开始完善O VECS 组
    
    罗瑶光

--- 2021/12/07/bloom/StaticFunctionMapA_IDUQ_E.java
+++ 2021/12/07/bloom/StaticFunctionMapA_IDUQ_E.java
@@ -1,5 +1,21 @@
 package SEM.bloom;
 
-public class StaticFunctionMapA_IDUQ_E {
+import OSI.OPE.SI.SD.SU.SQ.ASU.OSU.PSU.MSU.AVQ.ASQ.ASU.MPE.procedure.pde.FullDNATokenPDI_XCDX;
 
+public class StaticFunctionMapA_IDUQ_E {
+	@SuppressWarnings("unused")
+	public static void main(String[] argv) {
+		String string= "ยÞัþนโ";
+		String pdw=  StaticFunctionMapA_IDUQ_C.getPDW(string);
+		String lock= StaticFunctionMapA_IDUQ_C.getLock();
+		String code= StaticFunctionMapA_IDUQ_C.getCode(pdw, lock);
+		FullDNATokenPDI_XCDX pDE_RNA_FullFormular= new FullDNATokenPDI_XCDX();
+		pDE_RNA_FullFormular.pdw= pdw.toString();
+		pDE_RNA_FullFormular.lock= lock.toString();
+		pDE_RNA_FullFormular.code= code.toString();
+		pDE_RNA_FullFormular= StaticFunctionMapA_IDUQ_C.doPDE(pDE_RNA_FullFormular);
+		FullDNATokenPDI_XCDX pDE_RNA_FullFormular1= StaticFunctionMapA_IDUQ_C.doPrefixPDE(pDE_RNA_FullFormular);
+		FullDNATokenPDI_XCDX pDE_RNA_FullFormular2= StaticFunctionMapA_IDUQ_C.doPostfixPDE(pDE_RNA_FullFormular, pDE_RNA_FullFormular1);
+		FullDNATokenPDI_XCDX pDE_RNA_FullFormular3= StaticFunctionMapA_IDUQ_C.doSurffixPDE(pDE_RNA_FullFormular, pDE_RNA_FullFormular1);
+	} 
 }
