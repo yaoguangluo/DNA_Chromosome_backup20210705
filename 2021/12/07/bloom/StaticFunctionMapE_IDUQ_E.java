c8e4206fa8cd2ccfeba26fc4b4274aa3fe0c45ac
Deta Open Source <2080315360@qq.com>
2021-12-16 19:59:01 +0800

    稍后开始封装 非常用的子函数元基组
    1 Eiduq组 的 ETL组件函数 封装了下。
    2 准备将我的官网 https://tinos.qicp.vip 服务器用上这朵元基花，今晚。
    
    
    罗瑶光

--- 2021/12/07/bloom/StaticFunctionMapE_IDUQ_E.java
+++ 2021/12/07/bloom/StaticFunctionMapE_IDUQ_E.java
@@ -1,5 +1,237 @@
 package SEM.bloom;
 
-public class StaticFunctionMapE_IDUQ_E {
+import java.awt.PopupMenu;
+import java.io.IOException;
+import java.util.Map;
+
+import javax.swing.JTextPane;
+
+import ME.APM.VSQ.App;
+import OCI.ME.analysis.C.A;
+import OSI.OPE.AOPM.VECS.IDUQ.OVU.PQE.flash.GUISample;
+import OSI.OPE.MSQ.OVU.PQE.nodeInfo.NodeInfo;
+import OSI.OPE.MSQ.OVU.PQE.nodeProject.NodeProject;
+import OSI.OPE.MVU.OVU.OVU.PQE.nodeEdit.C.CheckRange;
+import OSI.OPE.MVU.OVU.OVU.PQE.nodeEdit.C.ChooseCheck;
+import OSI.OPE.MVU.OVU.OVU.PQE.nodeEdit.C.DrawArrow;
+import OSI.OPE.MVU.OVU.OVU.PQE.nodeEdit.C.DynamicLine_U;
+import OSI.OPE.MVU.OVU.OVU.PQE.nodeEdit.C.Sort;
+import OSI.OPE.MVU.OVU.OVU.PQE.nodeEdit.C.U_RelatedLine;
+import OSI.OPE.OEQ.MCQ.OVU.PQE.osgi.LinkOSGI;
+import OSI.OPE.OEQ.MCQ.OVU.PQE.osgi.NodeOSGI;
+import OSI.OPE.OEQ.MCQ.OVU.PQE.osgi.OSU_AVQ_ASQ_ASQ_OCQ_OSI_PCI_PCU_MCI_MCU_MSI;
+import OSI.OPE.OEQ.MCQ.OVU.PQE.osgi.OSU_AVQ_ASQ_AVQ_ASQ_OVQ_OSQ_VSQ;
+import OSI.OPE.OEQ.MCQ.OVU.PQE.osgi.OSU_AVQ_ASQ_OPE_OPC_ECI;
+import OSI.OPE.OVQ.MSQ.OVU.PQE.platForm.UnicornJSplitPane;
+import OSI.OPE.OVQ.MSQ.OVU.PQE.platForm.UnicornScrollBarUI;
+import OSI.OPE.OVQ.MSQ.OVU.PQE.platForm.UnicornSplitPaneUI;
+import OSI.OPE.OVQ.MSQ.OVU.PQE.platForm.UnicornTreeCellRenderer;
+import OSI.OPE.OVQ.MSQ.OVU.PQE.platForm.UnicornTreeUI;
+import OSI.OPE.OVU.MVQ.OVU.PQE.nodeView.CacuString;
+import OSI.OPE.OVU.MVQ.OVU.PQE.nodeView.NodeShow;
+import OSI.OPE.OVU.MVU.OVU.PQE.nodeEdit.CheckRangeVPS;
+import OSI.OPE.OVU.MVU.OVU.PQE.nodeEdit.ChooseQ_VPS;
+import OSI.OPE.OVU.MVU.OVU.PQE.nodeEdit.DrawArrowVPS;
+import OSI.OPE.OVU.MVU.OVU.PQE.nodeEdit.DrawFlashSide;
+import OSI.OPE.OVU.MVU.OVU.PQE.nodeEdit.DrawSinLineVPS;
+import OSI.OPE.OVU.MVU.OVU.PQE.nodeEdit.DynamicLineU_VPS;
+import OSI.OPE.OVU.MVU.OVU.PQE.nodeEdit.LinkList;
+import OSI.OPE.OVU.MVU.OVU.PQE.nodeEdit.LinkNode;
+import OSI.OPE.OVU.MVU.OVU.PQE.nodeEdit.U_RelatedLineVPS;
+import OSI.OPE.SI.MCI.OEI.OVU.PQE.extOSGI.OSGI_chansfer;
+import OSI.OPE.SI.MCI.OEI.OVU.PQE.extOSGI.OSI_OSU_ASQ_OCQ_OSI_PCI_PCU_MCI_MCU_MSI_register;
+
+//著作权人+ 作者= 罗瑶光
+//Refer的源码来自 《DNA 元基催化与肽计算 第三修订版本 V039010912》
+//证书编号：国作登字-2021-L-00268255 (中华人民共和国 国家版权登记中心)
+public class StaticFunctionMapE_IDUQ_E implements StaticFunctionMapE_IDUQ_C{
+
+	@Override
+	public NodeInfo getNodeInfo() {
+		// TODO Auto-generated method stub
+		return new NodeInfo();
+	}
+
+	@Override
+	public NodeProject getNodeProject() {
+		// TODO Auto-generated method stub
+		return new NodeProject();
+	}
+
+	@Override
+	public CheckRange getCheckRange() {
+		// TODO Auto-generated method stub
+		return new CheckRangeVPS();
+	}
+
+	@Override
+	public ChooseCheck getChooseCheck() {
+		// TODO Auto-generated method stub
+		return new ChooseQ_VPS();
+	}
+
+	@Override
+	public DrawArrow getDrawArrow() {
+		// TODO Auto-generated method stub
+		return new DrawArrowVPS();
+	}
+
+	@Override
+	public DynamicLine_U getDynamicLine_U() {
+		// TODO Auto-generated method stub
+		return new DynamicLineU_VPS();
+	}
+
+	@Override
+	public U_RelatedLine getU_RelatedLine() {
+		// TODO Auto-generated method stub
+		return new U_RelatedLineVPS();
+	}
+
+	@Override
+	public LinkOSGI getLinkOSGI() {
+		// TODO Auto-generated method stub
+		return new LinkOSGI();
+	}
+
+	@Override
+	public NodeOSGI getNodeOSGI() {
+		// TODO Auto-generated method stub
+		return new NodeOSGI();
+	}
+
+	@Override
+	public OSU_AVQ_ASQ_ASQ_OCQ_OSI_PCI_PCU_MCI_MCU_MSI _OSU_AVQ_ASQ_ASQ_OCQ_OSI_PCI_PCU_MCI_MCU_MSI() {
+		// TODO Auto-generated method stub
+		return new OSU_AVQ_ASQ_ASQ_OCQ_OSI_PCI_PCU_MCI_MCU_MSI();
+	}
+
+	@Override
+	public OSU_AVQ_ASQ_AVQ_ASQ_OVQ_OSQ_VSQ _OSU_AVQ_ASQ_AVQ_ASQ_OVQ_OSQ_VSQ() {
+		// TODO Auto-generated method stub
+		return new OSU_AVQ_ASQ_AVQ_ASQ_OVQ_OSQ_VSQ();
+	}
+
+	@Override
+	public OSU_AVQ_ASQ_OPE_OPC_ECI _OSU_AVQ_ASQ_OPE_OPC_ECI() {
+		// TODO Auto-generated method stub
+		return new OSU_AVQ_ASQ_OPE_OPC_ECI();
+	}
+
+	@Override
+	public UnicornJSplitPane getUnicornJSplitPane() {
+		// TODO Auto-generated method stub
+		return new UnicornJSplitPane();
+	}
+
+	@Override
+	public UnicornScrollBarUI getUnicornScrollBarUI() {
+		// TODO Auto-generated method stub
+		return new UnicornScrollBarUI();
+	}
+
+	@Override
+	public UnicornSplitPaneUI getUnicornSplitPaneUI() {
+		// TODO Auto-generated method stub
+		return new UnicornSplitPaneUI();
+	}
+
+	@Override
+	public UnicornTreeCellRenderer getUnicornTreeCellRenderer() {
+		// TODO Auto-generated method stub
+		return new UnicornTreeCellRenderer();
+	}
+
+	@Override
+	public UnicornTreeUI getUnicornTreeUI() {
+		// TODO Auto-generated method stub
+		return new UnicornTreeUI();
+	}
+
+	@Override
+	public CacuString getCacuString() {
+		// TODO Auto-generated method stub
+		return new CacuString();
+	}
+
+	@Override
+	public NodeShow getNodeShow(GUISample gUISample, Object[][] tableData_old, JTextPane text
+			, PopupMenu popupMenu, App u, A _A, Map<String, String> pos) throws IOException {
+		// TODO Auto-generated method stub
+		return new NodeShow(gUISample, tableData_old, text, popupMenu, u, _A, pos);
+	}
+
+	@Override
+	public CheckRangeVPS getCheckRangeVPS() {
+		// TODO Auto-generated method stub
+		return new CheckRangeVPS();
+	}
+
+	@Override
+	public ChooseQ_VPS getChooseQ_VPS() {
+		// TODO Auto-generated method stub
+		return new ChooseQ_VPS();
+	}
+
+	@Override
+	public DrawArrowVPS getDrawArrowVPS() {
+		// TODO Auto-generated method stub
+		return new DrawArrowVPS();
+	}
+
+	@Override
+	public DrawFlashSide getDrawFlashSide() {
+		// TODO Auto-generated method stub
+		return new DrawFlashSide();
+	}
+
+	@Override
+	public DrawSinLineVPS getDrawSinLineVPS() {
+		// TODO Auto-generated method stub
+		return new DrawSinLineVPS();
+	}
+
+	@Override
+	public DynamicLineU_VPS getDynamicLineU_VPS() {
+		// TODO Auto-generated method stub
+		return new DynamicLineU_VPS();
+	}
+
+	@Override
+	public LinkList getLinkList() {
+		// TODO Auto-generated method stub
+		return new LinkList();
+	}
+
+	@Override
+	public LinkNode getLinkNode() {
+		// TODO Auto-generated method stub
+		return new LinkNode();
+	}
+
+	@Override
+	public Sort getSort() {//稍后
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public U_RelatedLineVPS getU_RelatedLineVPS() {
+		// TODO Auto-generated method stub
+		return new U_RelatedLineVPS();
+	}
+
+	@Override
+	public OSGI_chansfer getOSGI_chansfer(LinkNode node, LinkNode first) {
+		// TODO Auto-generated method stub
+		return new OSGI_chansfer(node, first);
+	}
+
+	@Override
+	public OSI_OSU_ASQ_OCQ_OSI_PCI_PCU_MCI_MCU_MSI_register _OSI_OSU_ASQ_OCQ_OSI_PCI_PCU_MCI_MCU_MSI_register(Object[][] tableData_old
+			, JTextPane text, App u, A _A, Map<String, String> pos) {
+		// TODO Auto-generated method stub
+		return new OSI_OSU_ASQ_OCQ_OSI_PCI_PCU_MCI_MCU_MSI_register(tableData_old, text, u, _A, pos);
+	}
 
 }
