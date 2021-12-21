6cde72505465c0bc1f3f7aae2e4f55b1f45a26da
Deta Open Source <2080315360@qq.com>
2021-12-21 14:38:07 +0800

    DNA索引元基花 的 轮询接口执行虚拟机。
    1 测试下main通过了，稍后设计传参。
    
    罗瑶光

--- 2021/12/07/bloom/StaticFunctionMap.java
+++ 2021/12/07/bloom/StaticFunctionMap.java
@@ -1,17 +1,73 @@
 package SEM.bloom;
 
-import java.util.Map;
+import java.io.IOException;
+
 //用来索引函数 注册类
 //罗瑶光
 public class StaticFunctionMap{
-	public static Map<String, Object>  staticFunctionMap;
-	public void initMap() {
-		//1 准备开始设计 元基static 索引map
-		//2 DNA 元基索引函数 记忆配对模型 已经确定。
-		//3 稍后扩充函数，是static的直接载入，不是的 就把 是接口函数的进行VECS分离，把E接口载入。
-
-
-		//罗瑶光
+	public static void doA_VECS_CaseFunction(String callFunctionKey, String string, StaticFunctionMapA_VECS_E staticFunctionMapA_VECS_C) throws IOException {
+		if(callFunctionKey.equalsIgnoreCase("main")) {
+			//....
+		}
+	}
+	public static void doA_IDUQ_CaseFunction(String callFunctionKey, String string, StaticFunctionMapA_IDUQ_E staticFunctionMapA_IDUQ_C) throws IOException {
+	}
+	public static void doO_VECS_CaseFunction(String callFunctionKey, String string, StaticFunctionMapO_VECS_E staticFunctionMapO_VECS_C) throws IOException {
+	}
+	public static void doO_IDUQ_CaseFunction(String callFunctionKey, String string, StaticFunctionMapO_IDUQ_E staticFunctionMapO_IDUQ_C) throws IOException {
+	}
+	public static void doP_VECS_CaseFunction(String callFunctionKey, String string, StaticFunctionMapP_VECS_E staticFunctionMapP_VECS_C) throws IOException {
+	}
+	public static void doP_IDUQ_CaseFunction(String callFunctionKey, String string, StaticFunctionMapP_IDUQ_E staticFunctionMapP_IDUQ_C) throws IOException {
+	}
+	public static void doM_VECS_CaseFunction(String callFunctionKey, String string, StaticFunctionMapM_VECS_E staticFunctionMapM_VECS_C) throws IOException {
+	}
+	public static void doM_IDUQ_CaseFunction(String callFunctionKey, String string, StaticFunctionMapM_IDUQ_E staticFunctionMapM_IDUQ_C) throws IOException {
+	}
+	public static void doV_AOPM_CaseFunction(String callFunctionKey, String string, StaticFunctionMapV_AOPM_E staticFunctionMapV_AOPM_C) throws IOException {
+	}
+	public static void doV_IDUQ_CaseFunction(String callFunctionKey, String string, StaticFunctionMapV_IDUQ_E staticFunctionMapV_IDUQ_C) throws IOException {
+	}
+	public static void doE_AOPM_CaseFunction(String callFunctionKey, String string, StaticFunctionMapE_AOPM_E staticFunctionMapE_AOPM_C) throws IOException {
+	}
+	public static void doE_IDUQ_CaseFunction(String callFunctionKey, String string, StaticFunctionMapE_IDUQ_E staticFunctionMapE_IDUQ_C) throws IOException {
+	}
+	public static void doC_AOPM_CaseFunction(String callFunctionKey, String string, StaticFunctionMapC_AOPM_E staticFunctionMapC_AOPM_C) throws IOException {
 	}
 	
+	public static void doC_IDUQ_CaseFunction(String callFunctionKey, String string, StaticFunctionMapC_IDUQ_E staticFunctionMapC_IDUQ_C) throws IOException {
+	}
+	public static void doS_AOPM_CaseFunction(String callFunctionKey, String string, StaticFunctionMapS_AOPM_E staticFunctionMapS_AOPM_C) throws IOException {
+	}
+	public static void doS_IDUQ_CaseFunction(String callFunctionKey, String string, StaticFunctionMapS_IDUQ_E staticFunctionMapS_IDUQ_C) throws IOException {
+	}
+	public static void doI_AOPM_CaseFunction(String callFunctionKey, String string, StaticFunctionMapI_AOPM_E staticFunctionMapI_AOPM_C) throws IOException {
+	}
+	public static void doI_VECS_CaseFunction(String callFunctionKey, String string, StaticFunctionMapI_VECS_E staticFunctionMapI_VECS_C) throws IOException {
+	}
+	public static void doD_AOPM_CaseFunction(String callFunctionKey, String string, StaticFunctionMapD_AOPM_E staticFunctionMapD_AOPM_C) throws IOException {
+	}
+	public static void doD_VECS_CaseFunction(String callFunctionKey, String string, StaticFunctionMapD_VECS_E staticFunctionMapD_VECS_C) throws IOException {
+	}
+	public static void doU_AOPM_CaseFunction(String callFunctionKey, String string, StaticFunctionMapU_AOPM_E staticFunctionMapU_AOPM_C) throws IOException {
+	}
+	
+	@SuppressWarnings("static-access")
+	public static void doU_VECS_CaseFunction(String callFunctionKey, String string, StaticFunctionMapU_VECS_E staticFunctionMapU_VECS_C) throws IOException {
+		if(callFunctionKey.equalsIgnoreCase("main")) {//稍后分出去
+			//写法1
+			//case 参数
+			if(string.contains("null")) {
+				//其他参数可用object， json 都可以
+				staticFunctionMapU_VECS_C.main(null);//稍后分出去
+			}
+			//写法2
+			//可以插件遍历，可以 接口遍历，可以web的outowire 遍历，
+			//无数种方法遍历
+		}
+	}
+	public static void doQ_AOPM_CaseFunction(String callFunctionKey, String string, StaticFunctionMapQ_AOPM_E staticFunctionMapQ_AOPM_C) throws IOException {
+	}
+	public static void doQ_VECS_CaseFunction(String callFunctionKey, String string, StaticFunctionMapQ_VECS_E staticFunctionMapQ_VECS_C) throws IOException {
+	}
 }
