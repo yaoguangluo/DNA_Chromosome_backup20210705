3d7f7ac3ce0763c2c17a4a3661da19dd8cfbf000
Deta Open Source <2080315360@qq.com>
2021-12-31 06:49:00 +0800

    漏了一组，齐了
    罗瑶光

--- 2021/12/07/bloom/StaticFunctionMapP_IDUQ_C.java
+++ 2021/12/07/bloom/StaticFunctionMapP_IDUQ_C.java
@@ -11,14 +11,16 @@ import OSI.OPE.SI.SD.SU.SQ.ASU.OSU.PSU.MSU.AVQ.ASQ.dna.P.RST;
 //终于开始快要派上用场了。我的初衷。
 public interface StaticFunctionMapP_IDUQ_C {
 	@SuppressWarnings("unchecked")
-	public static void callFunction(String callFunctionKey, StaticFunctionMapP_IDUQ_E staticFunctionMapP_IDUQ_C, Map<String, Object> output) throws IOException {
+	public static void callFunction(String callFunctionKey, StaticFunctionMapP_IDUQ_E staticFunctionMapP_IDUQ_C
+			, Map<String, Object> output) throws IOException {
 		String[] 传参因子= (String[]) output.get("传参因子");
 		int 因子= 0;
 		Object map = null;
 		if(callFunctionKey.equalsIgnoreCase("copyMap")){ 
 			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, 传参因子);
 			if((boolean) inputValues.get("find")) {
-				map= staticFunctionMapP_IDUQ_C.copyMap((ConcurrentLinkedDeque<PDN>)inputValues.get(传参因子[因子++]));
+				map= staticFunctionMapP_IDUQ_C.copyMap(
+						(ConcurrentLinkedDeque<PDN>)inputValues.get(传参因子[因子++]));
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
@@ -26,7 +28,8 @@ public interface StaticFunctionMapP_IDUQ_C {
 			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, 传参因子);
 			if((boolean) inputValues.get("find")) {
 				map= staticFunctionMapP_IDUQ_C.mask((ConcurrentLinkedDeque<PDN>)inputValues.get(传参因子[因子++])
-						, (ConcurrentLinkedDeque<PDN>) inputValues.get(传参因子[因子++]), (ConcurrentLinkedDeque<PDN>) inputValues.get(传参因子[因子++]));
+						, (ConcurrentLinkedDeque<PDN>) inputValues.get(传参因子[因子++])
+						, (ConcurrentLinkedDeque<PDN>) inputValues.get(传参因子[因子++]));
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
@@ -47,14 +50,16 @@ public interface StaticFunctionMapP_IDUQ_C {
 		if(callFunctionKey.equalsIgnoreCase("dating")){ 
 			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, 传参因子);
 			if((boolean) inputValues.get("find")) {
-				map= staticFunctionMapP_IDUQ_C.dating((RST)inputValues.get(传参因子[因子++]), (RST)inputValues.get(传参因子[因子++]));
+				map= staticFunctionMapP_IDUQ_C.dating((RST)inputValues.get(传参因子[因子++])
+						, (RST)inputValues.get(传参因子[因子++]));
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
 		if(callFunctionKey.equalsIgnoreCase("inheritance")){ 
 			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, 传参因子);
 			if((boolean) inputValues.get("find")) {
-				map= staticFunctionMapP_IDUQ_C.inheritance((RST)inputValues.get(传参因子[因子++]), (RST)inputValues.get(传参因子[因子++]));
+				map= staticFunctionMapP_IDUQ_C.inheritance((RST)inputValues.get(传参因子[因子++])
+						, (RST)inputValues.get(传参因子[因子++]));
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
@@ -68,7 +73,8 @@ public interface StaticFunctionMapP_IDUQ_C {
 		if(callFunctionKey.equalsIgnoreCase("mutation")){ 
 			Map<String, Object> inputValues= StaticFunctionMap.preValues(output, 传参因子);
 			if((boolean) inputValues.get("find")) {
-				map= staticFunctionMapP_IDUQ_C.mutation((RST)inputValues.get(传参因子[因子++]), (RST)inputValues.get(传参因子[因子++]));
+				map= staticFunctionMapP_IDUQ_C.mutation((RST)inputValues.get(传参因子[因子++])
+						, (RST)inputValues.get(传参因子[因子++]));
 			}
 			StaticFunctionMap.postValues(output, (boolean) inputValues.get("find"), map, callFunctionKey);
 		};
@@ -91,18 +97,18 @@ public interface StaticFunctionMapP_IDUQ_C {
 		// TODO Auto-generated method stub
 		//稍后封装
 
-		staticFunctionMapP_IDUQ_E.annotationMap.put("copy", "copy");
-		staticFunctionMapP_IDUQ_E.annotationMap.put("copyMap", "copyMap");
-		staticFunctionMapP_IDUQ_E.annotationMap.put("mask", "mask");
-		staticFunctionMapP_IDUQ_E.annotationMap.put("copy", "copy");
+		staticFunctionMapP_IDUQ_E.annotationMap.put("copy", "dna");
+		staticFunctionMapP_IDUQ_E.annotationMap.put("copyMap", "functionLineDeque");
+		staticFunctionMapP_IDUQ_E.annotationMap.put("mask", "originDeque:newDeque:maskOutput");
+		staticFunctionMapP_IDUQ_E.annotationMap.put("copy", "maskLineDeque");
 
-		staticFunctionMapP_IDUQ_E.annotationMap.put("mutationCopy", "mutationCopy");	
-		staticFunctionMapP_IDUQ_E.annotationMap.put("dating", "dating");	
-		staticFunctionMapP_IDUQ_E.annotationMap.put("inheritance", "inheritance");	
-		staticFunctionMapP_IDUQ_E.annotationMap.put("born", "born");	
-		staticFunctionMapP_IDUQ_E.annotationMap.put("mutation", "mutation");	
-		staticFunctionMapP_IDUQ_E.annotationMap.put("separater", "separater");	
-		staticFunctionMapP_IDUQ_E.annotationMap.put("swap", "swap");	
+		staticFunctionMapP_IDUQ_E.annotationMap.put("mutationCopy", "dna");	
+		staticFunctionMapP_IDUQ_E.annotationMap.put("dating", "father:mother");	
+		staticFunctionMapP_IDUQ_E.annotationMap.put("inheritance", "father:mother");	
+		staticFunctionMapP_IDUQ_E.annotationMap.put("born", "void");	
+		staticFunctionMapP_IDUQ_E.annotationMap.put("mutation", "father:mother");	
+		staticFunctionMapP_IDUQ_E.annotationMap.put("separater", "original");	
+		staticFunctionMapP_IDUQ_E.annotationMap.put("swap", "original");	
 	}
 
 	// CopyLaw{
